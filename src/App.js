3021179294a10be338a0c41c2124a61ae25bf431
prasunbhunia <bhunia.prasun99@gmail.com>
2021-01-09 18:23:47 +0530

    Added files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import HomePage from './homepage';
+
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
